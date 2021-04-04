a892628a4a875d6cb4156613fe3ed46488e74d7f
Zayyed Mansoor <zayyed.not@gmail.com>
2021-04-04 16:53:11 -0400

    new instructor registration flow

--- frontend/angularFrontend/src/app/signup-page/signup-page.component.ts
+++ frontend/angularFrontend/src/app/signup-page/signup-page.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { ApplicationService } from "../application.service";
-import {Router} from "@angular/router"
+import { ApplicationService } from '../application.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
+  styleUrls: ['./signup-page.component.css'],
 })
 export class SignupPageComponent implements OnInit {
+  role: string = localStorage.getItem('role');
 
+  constructor(
+    private router: Router,
+    private applicationService: ApplicationService
+  ) {}
 
+  ngOnInit(): void {}
 
-  constructor(private router: Router, private applicationService: ApplicationService) { }
+  signup(data) {
+    console.log(data);
 
-  ngOnInit(): void {
+    this.applicationService.signup(data).subscribe((res) => {
+      console.log(res);
+
+      if(!localStorage.getItem('role')) {
+        localStorage.getItem('role')
+        localStorage.setItem('role', res.role);
+        localStorage.setItem('email', res.email);
+        localStorage.setItem('userID', res.userID);
+        localStorage.setItem('_id', res._id);
+        this.router.navigate(['landing']).then(() => window.location.reload());
+      }
+    });
   }
-
-  signup(data){
-    console.log(data)
-
-    this.applicationService.signup(data).subscribe(res=>{
-      console.log(res)
-      localStorage.setItem('role', res.role);
-      localStorage.setItem('email', res.email);
-      localStorage.setItem('userID', res.userID);
-      localStorage.setItem('_id', res._id);
-      this.router.navigate(['landing']).then(()=>window.location.reload())
-    } )
-  }
-
 }
